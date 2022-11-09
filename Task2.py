b6b25a8848bd82cd26de5c4c8af656dfef7f7636
Hunchovelli <jordanmensah13@gmail.com>
2022-11-09 14:06:05 +0000

    Lines Addition

--- Task2.py
+++ Task2.py
@@ -2,16 +2,21 @@
 
 #importing the module which read excel files
 from openpyxl import load_workbook
+import networkx
+import matplotlib.pyplot as plt
 
 #creates an object to the excel sheet with the london undeground data
 file = load_workbook("Stations.xlsx") #make sure to run python script in the working directory
 #stores the main sheet in variable
 sheet = file.active
 
+G = networkx.Graph()
+
 #List of all the stations without duplicates
 stations_list = []
-#Dictionary of stations and the lines that run through them
+#Temporary dictionary of stations and the neighbouring stations
 stations_dict = {}
+#Permanent dictionary of stations and the neighbouring stations
 
 #iterates through the column 'B' in the excel sheet
 for station in sheet["B"]:
@@ -20,7 +25,6 @@ for station in sheet["B"]:
         #adds a station to the list if not already in the list
         stations_list.append(station.value)
 
-#iterates through the station_list (without duplicates) so we can add all the lines wach station goes through as values in a dictionary
 for station in stations_list:
     #iterates through the column 'B' in the excel sheet
     for cell in sheet["B"]:
@@ -29,10 +33,53 @@ for station in stations_list:
             #appends to dictionary the corresponding value in column A and creates empty liste with the key as a default if the key is not present yet
             stations_dict.setdefault(station, []).append(sheet['A'+str(cell.row)].value)
 
-#iterate thorugh the dictionary to see which stations have only one line       
-for station in stations_dict:
-    #checks if station/value has only one line/value
-    if len(stations_dict[station]) == 1:
-        #prints conclusion that it is not feasible to close station as only one line goes through it
-        print ('Closure of ' + str(station) + ' is not feasible as it only goes through one line which is the ' + str(stations_dict[station])
-        + ' hence it will be inaccesible from other lines if the station is closed.')  
+
+
+#makes a station to station_list into node
+for station in stations_list:
+    G.add_node(station)
+
+
+#iterates through the station_list (without duplicates) so we can add all the lines each station goes through as values in a dictionary
+for station in stations_list:
+    #iterates through the column 'E' in the excel sheet
+    for cell in sheet["E"]:
+        #adds an edge between neighbouring stations on the graph
+        if cell.value == station:
+            G.add_edge(station, sheet['F'+str(cell.row)].value, weight = sheet['G'+str(cell.row)].value)
+            #print (temp_stations_dict)
+            #appends to dictionary the corresponding value in column A and creates empty liste with the key as a default if the key is not present yet
+
+#dijskta function with arguements
+def dijsktra(graph, source, destination):
+    #this used to defelect the exception error 
+    try:
+        #used to count the index of the line we are in 
+        count = 0
+        while networkx.has_path(graph, source, destination) == True:
+            graph.remove_edge(source, networkx.shortest_path(graph, source, destination)[1])
+            for station in networkx.shortest_path(graph, source, destination):
+                for cell in sheet["B"]:
+                    if cell.value == destination and sheet['B'+str(cell.row+1)].value == networkx.shortest_path(graph, source, destination)[count-1]:
+                        print (str(cell.value) + ' via the ' + sheet['A'+str(cell.row)].value + ' line.')
+                        break
+                for cell in sheet["B"]:        
+                    if station == cell.value and networkx.shortest_path(graph, source, destination)[count+1] == sheet['B'+str(cell.row-1)].value:
+                        print (str(cell.value) + ' via the ' + sheet['A'+str(cell.row)].value + ' line.')
+                count += 1
+            break
+    except:   
+        print ("No route is feasible as all other routes are closed or impossible")
+'''for cell in sheet["B"]:
+            if cell.value == destination and sheet['B'+str(cell.row+1)].value == route[count-1]:
+                print (str(cell.value) + ' via the ' + sheet['A'+str(cell.row)].value + ' line.')        
+        for cell in sheet["B"]:        
+            if station == cell.value and route[count+1] == sheet['B'+str(cell.row-1)].value:
+                print (str(cell.value) + ' via the ' + sheet['A'+str(cell.row)].value + ' line.')
+        count += 1'''        
+
+    
+src_input = input("Where would you like to start your journey?: ")
+dest_input = input("Where would you like to go to?: ")
+
+print (dijsktra(G, src_input, dest_input))
