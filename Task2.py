21444e80aaed9abe7c2b733a9649cc32d8010458
Hunchovelli <jordanmensah13@gmail.com>
2022-11-10 03:29:42 +0000

    Task 2a completed

--- Task2.py
+++ Task2.py
@@ -7,16 +7,14 @@ import matplotlib.pyplot as plt
 
 #creates an object to the excel sheet with the london undeground data
 file = load_workbook("Stations.xlsx") #make sure to run python script in the working directory
-#stores the main sheet in variable
+#stores the main sheet from the excel file in variable
 sheet = file.active
 
+#makes the graph object
 G = networkx.Graph()
 
 #List of all the stations without duplicates
 stations_list = []
-#Temporary dictionary of stations and the neighbouring stations
-stations_dict = {}
-#Permanent dictionary of stations and the neighbouring stations
 
 #iterates through the column 'B' in the excel sheet
 for station in sheet["B"]:
@@ -25,61 +23,72 @@ for station in sheet["B"]:
         #adds a station to the list if not already in the list
         stations_list.append(station.value)
 
+#for each station in station_list we iterate through the list
 for station in stations_list:
-    #iterates through the column 'B' in the excel sheet
-    for cell in sheet["B"]:
-        #matching the cell value in column B with the station to see how many time it occurs in column B to link it with the line it is on
-        if cell.value == station:
-            #appends to dictionary the corresponding value in column A and creates empty liste with the key as a default if the key is not present yet
-            stations_dict.setdefault(station, []).append(sheet['A'+str(cell.row)].value)
-
-
-
-#makes a station to station_list into node
-for station in stations_list:
+    #make each station into a node on the graph
     G.add_node(station)
 
 
-#iterates through the station_list (without duplicates) so we can add all the lines each station goes through as values in a dictionary
+#for each station in station_list we iterate through the list
 for station in stations_list:
-    #iterates through the column 'E' in the excel sheet
+    #iterates through every cell in column 'E' in the excel sheet
     for cell in sheet["E"]:
-        #adds an edge between neighbouring stations on the graph
+        #if the cell content we are currently on in the iteration of the excel sheet column 'E' is equal to the station we are currently on in the iteration of the station_list
         if cell.value == station:
-            G.add_edge(station, sheet['F'+str(cell.row)].value, weight = sheet['G'+str(cell.row)].value)
-            #print (temp_stations_dict)
-            #appends to dictionary the corresponding value in column A and creates empty liste with the key as a default if the key is not present yet
+            #make an edge between the current cell we are on in the excel sheet in column 'E' and the adjacent cell in colum 'F'. Also, add the attributes weight/time between the stations which is in column 'G.
+            G.add_edge(cell.value, sheet['F'+str(cell.row)].value, weight = sheet['G'+str(cell.row)].value)
 
-#dijskta function with arguements
+#define function dijskta with arguements, graph = the graph of the stations, source = the starting station and destination = the target station.
 def dijsktra(graph, source, destination):
-    #this used to defelect the exception error 
+    #the counter/variable for the route different routes there are. Initialise variable at 1 to start from Route 1.
+    route_count = 1
+    #try block so we use the exception block to bypass the error message when there are no longer any possible routes
     try:
-        #used to count the index of the line we are in 
-        count = 0
+        #while loops which continues looping if it is True that there is a path between the source and destination stations on the graph
         while networkx.has_path(graph, source, destination) == True:
+            #the counter/variable for index of the list networkx.has_path(graph, source, destination), which returns an array of the journey, with the stations as the items.
+            count = 0
+            #the counter/variable for to store the time for each route
+            time = 0
+            #removes the edge between the starting station and the next station in networkx.has_path(graph, source, destination) path created.
             graph.remove_edge(source, networkx.shortest_path(graph, source, destination)[1])
+            #labels the route, starting from route 1 and utilise the route_counter variable we initialised earlier.
+            print ("\nRoute %s\n" % (route_count))
+            #for loops through the items/stations in networkx.has_path(graph, source, destination) array.
             for station in networkx.shortest_path(graph, source, destination):
-                for cell in sheet["B"]:
-                    if cell.value == destination and sheet['B'+str(cell.row+1)].value == networkx.shortest_path(graph, source, destination)[count-1]:
-                        print (str(cell.value) + ' via the ' + sheet['A'+str(cell.row)].value + ' line.')
-                        break
-                for cell in sheet["B"]:        
-                    if station == cell.value and networkx.shortest_path(graph, source, destination)[count+1] == sheet['B'+str(cell.row-1)].value:
-                        print (str(cell.value) + ' via the ' + sheet['A'+str(cell.row)].value + ' line.')
+                #iterates through every cell in column 'B' in the excel sheet.
+                for cell in sheet['B']:
+                    #when we reach end of array, index error will be araised
+                    if station == destination:
+                        #hence use continue keyword to exit both for loop we are in and bypass the index error.
+                        continue
+                    #executes the code if the stations in the array we are looping in with the stations in the cell in column 'B' of the excel sheet and the next station in the array with the station row above in column B. This is to uniquely match the neighboruing stations with certain lines.
+                    if cell.value == station and networkx.shortest_path(graph, source, destination)[count+1] == sheet['B'+str(cell.row-1)].value:
+                        #prints the route in column form using newlines and formatting techniques
+                        print ("%s to %s via the %s line." % (cell.value, networkx.shortest_path(graph, source, destination)[count+1], sheet['A'+str(cell.row)].value))
+                        #adds the time from the values in column G for the route between the pair of stations in the same row in the excel sheet
+                        time += int(sheet['G'+str(cell.row)].value)
+                    #executes the code if the stations in the array we are looping in with the stations in the cell in column 'B' of the excel sheet and the next station in the array with the station row above in column B. This is to uniquely match the neighboruing stations with certain lines. 
+                    if cell.value == station and networkx.shortest_path(graph, source, destination)[count+1] == sheet['B'+str(cell.row+1)].value:
+                        #prints the route in column form using newlines and formatting techniques
+                        print ("%s to %s via the %s line." % (cell.value, networkx.shortest_path(graph, source, destination)[count+1], sheet['A'+str(cell.row)].value))
+                        #adds the time from the values in column G for the route between the pair of stations in the same row in the excel sheet
+                        time += int(sheet['G'+str(cell.row)].value)
+                #used to update the count variable after we have iterated through an item/station in the networkx.has_path(graph, source, destination) array.
                 count += 1
-            break
-    except:   
-        print ("No route is feasible as all other routes are closed or impossible")
-'''for cell in sheet["B"]:
-            if cell.value == destination and sheet['B'+str(cell.row+1)].value == route[count-1]:
-                print (str(cell.value) + ' via the ' + sheet['A'+str(cell.row)].value + ' line.')        
-        for cell in sheet["B"]:        
-            if station == cell.value and route[count+1] == sheet['B'+str(cell.row-1)].value:
-                print (str(cell.value) + ' via the ' + sheet['A'+str(cell.row)].value + ' line.')
-        count += 1'''        
+            #print the total time for each route after fully iterating through a networkx.has_path(graph, source, destination) array.
+            print ("\nTotal Time for Route %s is %s minutes." % (route_count, time))
+            #used to update the route_count variable after fully iterating through a networkx.has_path(graph, source, destination) array.
+            route_count += 1
+    #except block executed if networkx.exception.NetworkXNoPath is raised. Usually raised when no more possible paths from source to the target.  
+    except networkx.exception.NetworkXNoPath:
+        #instead of the default error message python produces when networkx.exception.NetworkXNoPath is raised, custom print statement is outputted instead.
+        print ("No route is feasible as all other routes are closed or impossible.")
 
-    
+#takes the input of starting station from the user
 src_input = input("Where would you like to start your journey?: ")
+#takes the input of destination station from the user
 dest_input = input("Where would you like to go to?: ")
 
+#executes the function defined above using the graph we created, the starting input from the user and the destination input from the user as the arguements.
 print (dijsktra(G, src_input, dest_input))
